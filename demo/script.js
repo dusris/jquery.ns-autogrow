b6e2fce1bd266ee41e94f887255dba18b872a681
dusris <chb@dusris.us>
2015-10-28 03:08:29 +0300

    Update script.js

--- demo/script.js
+++ demo/script.js
@@ -1,6 +1,6 @@
 $(function(){
-  $('.example1 textarea').autogrow();
-  $('.example2 textarea').autogrow({vertical: true, horizontal: false});
-  $('.example3 textarea').autogrow({vertical: false, horizontal: true});
-  $('.example4 textarea').autogrow({flickering: false});
+  $('.example1 textarea').css('overflow', 'hidden').autogrow();
+  $('.example2 textarea').css('overflow', 'hidden').autogrow({vertical: true, horizontal: false});
+  $('.example3 textarea').css('overflow', 'hidden').autogrow({vertical: false, horizontal: true});
+  $('.example4 textarea').css('overflow', 'hidden').autogrow({flickering: false});
 });
